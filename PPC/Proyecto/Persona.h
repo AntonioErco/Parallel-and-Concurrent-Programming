5f66950c170517685243486a389c50dce438660d
AntonioErco <43550650+AntonioErco@users.noreply.github.com>
2018-09-27 11:36:55 -0600

    Update Persona.h

--- PPC/Proyecto/Persona.h
+++ PPC/Proyecto/Persona.h
@@ -1,1 +1,27 @@
+#ifndef PPC_GoL_P // Programación Paralela y Concurrente - Conway's Game of Life
+#define PPC_GoL_P
+#include <iostream>
 
+using namespace std;
+
+enum Estado{Susceptible, Infectada, Muerta, Recuperada};
+
+class Persona {
+
+public:
+	Persona();
+	~Persona();
+
+	void setEstado(Estado est);
+	void setActivo(bool act);
+	Estado getEstado();
+	bool getActivo();
+
+
+
+private:
+	Estado estado;
+	bool activo;
+};
+
+#endif
