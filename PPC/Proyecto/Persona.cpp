e69a4f501a592235bd60d11207b066b6c2dc4532
AntonioErco <43550650+AntonioErco@users.noreply.github.com>
2018-09-27 11:36:34 -0600

    Update Persona.cpp

--- PPC/Proyecto/Persona.cpp
+++ PPC/Proyecto/Persona.cpp
@@ -1,1 +1,26 @@
 
+#include "Persona.h"
+
+Persona::Persona() {
+
+}
+
+Persona::~Persona() {
+
+}
+
+void Persona::setEstado(Estado est) {
+	estado = est;
+}
+
+void Persona::setActivo(bool act) {
+	activo = act;
+}
+
+Estado Persona::getEstado() {
+	return estado;
+}
+
+bool Persona::getActivo() {
+	return activo;
+}
