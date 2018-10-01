ea3b6248bb740252ba53a2786e37e312b0ad4014
AntonioErco <43550650+AntonioErco@users.noreply.github.com>
2018-10-01 01:47:50 -0600

    Update main.cpp

--- PPC/Proyecto/main.cpp
+++ PPC/Proyecto/main.cpp
@@ -1,1 +1,21 @@
+#include "Simulador.h"
 
+using namespace std;
+
+int main() {
+
+	Simulador *sim = new Simulador();
+	sim->iniciarMatriz();
+	int tics = 0;
+
+	cout << "Duración de la Simulación/Cantidad de Tics: " << endl;
+	cin >> tics;
+	for (int x = 0; x < tics; ++x) {
+		sim->actualizarMatriz();
+		sim->actualizarMatriz();
+	}
+
+	int n;
+	cin >> n;
+	return 0;
+}
