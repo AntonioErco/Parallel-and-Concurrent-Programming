099b1616980460b600a31ba96ad182e5e3dcf778
AntonioErco <43550650+AntonioErco@users.noreply.github.com>
2018-10-08 11:42:49 -0600

    Update main.cpp

--- PPC/Proyecto/main.cpp
+++ PPC/Proyecto/main.cpp
@@ -3,17 +3,16 @@
 using namespace std;
 
 int main() {
+
 	Simulador *sim = new Simulador();
 	sim->iniciarMatriz();
 	int tics = 0;
 	int hilos = 0;
 
-	cout << "Duracion de la Simulacion/Cantidad de Tics: " << endl;
-	cin >> tics;
 	cout << "Cantidad de Hilos: " << endl;
 	cin >> hilos;
 
-	sim->cantidadHilosYTics(hilos,tics);
+	sim->cantidadHilos(hilos);
 	string output = sim->actualizarMatriz();
 
 	cout << output << endl;
