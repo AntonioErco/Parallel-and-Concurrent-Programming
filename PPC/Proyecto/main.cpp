3a21ad62c953658c576b20cf54a3570251fafa3f
AntonioErco <43550650+AntonioErco@users.noreply.github.com>
2018-10-08 14:30:56 -0600

    Update main.cpp

--- PPC/Proyecto/main.cpp
+++ PPC/Proyecto/main.cpp
@@ -1,26 +1,36 @@
+#include <chrono>
 #include "Simulador.h"
 
 using namespace std;
 
 int main() {
+	using namespace std::chrono;
 
-	Simulador *sim = new Simulador();
-	sim->iniciarMatriz();
-	int tics = 0;
+	int x = 0;
 	int hilos = 0;
-
 	cout << "Cantidad de Hilos: " << endl;
 	cin >> hilos;
 
-	sim->cantidadHilos(hilos);
-	string output = sim->actualizarMatriz();
+	while (x == 0) {
+		Simulador *sim = new Simulador();
 
-	cout << output << endl;
-	ofstream out("C:/Users/ExtremeTech/source/repos/Proyecto/dias.txt");
-	out << output;
-	out.close();
+		steady_clock::time_point t1 = steady_clock::now();
 
-	int n;
-	cin >> n;
+		sim->iniciarMatriz();
+
+		sim->cantidadHilos(hilos);
+		string output = sim->actualizarMatriz();
+
+		cout << output << endl;
+		ofstream out("C:/Users/ExtremeTech/source/repos/Proyecto/dias.txt");
+		out << output;
+		out.close();
+
+		steady_clock::time_point t2 = steady_clock::now();
+		duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+		std::cout << "Duracion " << time_span.count() << " segundos.";
+		cout << "\n 0 para continuar" << endl;
+		cin >> x;
+	}
 	return 0;
 }
